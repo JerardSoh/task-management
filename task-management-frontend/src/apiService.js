75cec1264a7cfdb29487255c389f2d35e1b07156
JerardSoh <jerard.soh@gmail.com>
2024-06-21 16:10:27 +0800

    Changes

--- task-management-frontend/src/apiService.js
+++ task-management-frontend/src/apiService.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL || "/api",
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
 export const fetchData = async () => {
